4743f0236df6500d2c6d0d8baf19174750c829e4
CSTSTEVENU <000455287@student.vcc.ca>
2022-09-10 09:59:43 -0700

    Exercise1
    CSTP 2104 exercise

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Exercise1_CSTP2104
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public class Organization
+        {
+            // data members 
+            private string name;
+            private string jobTitle;
+            private int daysWorked;
+            private int salary;
+
+            public string Name { get; set; }
+            public string JobTitle { get; set; }
+
+            public int DaysWorked { get; set; }
+            public int Salary { get; set; }
+
+            public Organization(string name, string jobTitle, int daysWorked, int salary)
+            {
+                this.name = name;
+                this.jobTitle = jobTitle;
+                this.daysWorked = daysWorked;
+                this.salary = salary; 
+            }
+
+        }
+
+        public class projectManger : Organization
+        {
+            public projectManger(string name, string jobTitle, int daysWorked, int salary) : base( name, jobTitle, daysWorked, salary)
+            {
+
+            }
+        }
+
+        public class developer : Organization
+        {
+            public developer(string name, string jobTitle, int daysWorked, int salary) : base(name, jobTitle, daysWorked, salary)
+            {
+
+            }
+        }
+    }
+}
