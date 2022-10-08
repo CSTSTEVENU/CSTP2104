64f59dc50a8befdd78240f22a69f3a8c5a9a846b
CSTSTEVENU <91871545+CSTSTEVENU@users.noreply.github.com>
2022-10-08 11:17:54 -0700

    quiz upload
    uploading quiz

---
+++ 2104-Sep2022/Quiz_Delegates/SalaryCalculator2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2104_Sep2022.Quiz_Delegates
+{
+    public class SalaryCalculator2
+    {
+        delegate double Converter(int num);
+        
+        public void SalaryDelegate()
+        {
+            Converter converter = multiplyBySalary;
+        }
+
+        public int addBySalary(int i)
+        {
+            return i + i;
+        }
+
+        public double multiplyBySalary(double i)
+        {
+            return i * i;
+        }
+
+        public string getString(string i)
+        {
+            return i;
+        }
+    }
+}
